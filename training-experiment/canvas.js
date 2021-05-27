c7c331fb33c0485b65debfdc38a91acdbc730527
alois <alois@in-temp.ch>
2021-05-27 11:57:37 +0200

    update

--- training-experiment/canvas.js
+++ training-experiment/canvas.js
@@ -8,27 +8,28 @@ canvas.width = window.innerWidth;
 // canvas.width = window.innerHeight / 18 * 9;
 
 window.addEventListener('resize', function() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 });
 
 //Mouse tracker
 var mouse = {
-  x: undefined,
-  y: undefined
+    x: undefined,
+    y: undefined
 }
 window.addEventListener('mousemove', function(e) {
-  mouse.x = e.x;
-  mouse.y = e.y;
+    mouse.x = e.x;
+    mouse.y = e.y;
 });
 //colors
 var colorPallet = [
-  '#0DA7FF',
-  '#0CD2E8',
-  '#00FFD2',
-  '#0CE883',
-  'rgba(0, 0, 0, 0.5)'
+    '#0DA7FF',
+    '#0CD2E8',
+    '#00FFD2',
+    '#0CE883',
+    'rgba(0, 0, 0, 0.5)'
 ];
+
 
 // colorPallet[Math.floor(Math.random() * colorPallet.length)];
 
@@ -49,32 +50,32 @@ var colorPallet = [
 
 
 function init() {
-  Particle.list = [];
-  let r = 50;
-  let x = r + (Math.random() * (canvas.width - (r * 2)));
-  let y = r + (Math.random() * (canvas.height - (r * 2)));
-  let borderColor = colorPallet[Math.floor(Math.random() * colorPallet.length)];
-  let fillColor = colorPallet[Math.floor(Math.random() * colorPallet.length)];
-  new Particle(x, y, r, 5, 5, fillColor, borderColor);
-  intervalFunction(0);
+    Particle.list = [];
+
+    for (var i = 0; i < 33; i++) {
+        new Particle(5, 5);
+    }
+    intervalFunction(0);
 }
 
+
 function intervalFunction(time) {
-  const timeMultiplier = Math.min(((time - lastTime) / 16.67), 1);
-  lastTime = time;
-  c.rect(0, 0, canvas.width, canvas.height);
-  c.fillStyle = "rgba(50, 50, 50, 1)";
-  c.fill();
-  Particle.list.forEach(particle => {
-    particle.update(timeMultiplier);
-  });
-  window.requestAnimationFrame((time) => {
-    intervalFunction(time);
-  })
+    const timeMultiplier = Math.min(((time - lastTime) / 16.67), 1);
+    lastTime = time;
+    c.rect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = "rgba(50, 50, 50, 1)";
+    c.fill();
+
+    Particle.list.forEach(particle => {
+        particle.update(timeMultiplier);
+    });
+    window.requestAnimationFrame((time) => {
+        intervalFunction(time);
+    })
 }
 
 
 var lastTime = 0;
 document.addEventListener("DOMContentLoaded", function(event) {
-  init();
+    init();
 });
--- training-experiment/particle.js
+++ training-experiment/particle.js
@@ -1,40 +1,50 @@
 class Particle {
-  constructor(x, y, radius, velX, velY, fillColor, lineColor) {
-    this.x = x;
-    this.y = y;
-    this.r = radius;
-    this.border = 4;
-    this.velX = velX;
-    this.velY = velY;
-    this.fillColor = fillColor;
-    this.lineColor = lineColor;
-    Particle.list.push(this);
-  }
-
-  update(timeMultiplier) {
-    if (this.x >= canvas.width - this.r || this.x <= this.r) {
-      this.velX = -this.velX;
+    constructor(velX, velY) {
+        this.r = 10;
+        this.x = this.r + (Math.random() * (canvas.width - (this.r * 2)));
+        this.y = this.r + (Math.random() * (canvas.height - (this.r * 2)));
+        this.border = 4;
+        this.velX = (Math.random() * 100) - 9;
+        this.velY = (Math.random() * 100) - 5;
+        this.fillColor = colorPallet[Math.floor(Math.random() * colorPallet.length)];
+        this.lineColor = colorPallet[Math.floor(Math.random() * colorPallet.length)];
+        this.gravity = 0.5;
+        Particle.list.push(this);
     }
-    if (this.y >= canvas.height - this.r || this.y <= this.r) {
-      this.velY = -this.velY;
+
+    update(timeMultiplier) {
+
+        this.velY = this.velY + this.gravity
+
+        const moveX = this.velX * timeMultiplier;
+        const moveY = this.velY * timeMultiplier;
+        const targetX = this.x + moveX;
+        const targetY = this.y + moveY;
+
+        if (targetX >= canvas.width - this.r || targetX <= this.r) {
+            this.velX = -(this.velX * 0.95);
+        }
+        if (targetY >= canvas.height - this.r || targetY <= this.r) {
+            this.velY = -(this.velY * 0.95);
+        }
+
+        this.x = this.x + this.velX * timeMultiplier;
+        this.y = this.y + this.velY * timeMultiplier;
+
+        this.draw();
     }
-    this.x += this.velX * timeMultiplier;
-    this.y += this.velY * timeMultiplier;
 
-    this.draw();
-  }
+    draw() {
+        c.fillStyle = this.fillColor;
+        c.lineWidth = this.border;
+        c.strokeStyle = this.lineColor;
 
-  draw() {
-    c.fillStyle = this.fillColor;
-    c.lineWidth = this.border;
-    c.strokeStyle = this.lineColor;
+        c.beginPath();
+        c.arc(this.x, this.y, this.r, 0, Math.PI * 2);
 
-    c.beginPath();
-    c.arc(this.x, this.y, this.r, 0, Math.PI * 2);
-
-    c.stroke();
-    c.fill();
-  }
+        c.stroke();
+        c.fill();
+    }
 }
 
 Particle.list = [];
